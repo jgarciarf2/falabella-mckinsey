ea3519680f60e93b2f68f4e3b588fda190483544
jgarciarf2 <jgarciarf216@gmail.com>
2025-09-26 17:45:52 -0500

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-content">
-                <p>Powered by <strong><a href="https://github.com/jgarciarf2" target="_blank" rel="noopener noreferrer">jgarciarf2</a></strong></p>
+                <p>Powered by <strong><a style={{ color: '#3498db' }} href="https://github.com/jgarciarf2" target="_blank" rel="noopener noreferrer">jgarciarf2</a></strong></p>
             </div>
         </footer>
     );
